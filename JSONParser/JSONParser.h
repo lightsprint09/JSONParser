a0db3288d98001a64460ed7007b5beeee1e980e3
Lukas Schmidt <lukas.schmi@gmail.com>
2016-03-11 11:38:48 +0100

    Initial Commit

---
+++ JSONParser/JSONParser.h
@@ -0,0 +1,19 @@
+//
+//  JSONParser.h
+//  JSONParser
+//
+//  Created by Lukas Schmidt on 11.03.16.
+//  Copyright © 2016 Lukas Schmidt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONParser.
+FOUNDATION_EXPORT double JSONParserVersionNumber;
+
+//! Project version string for JSONParser.
+FOUNDATION_EXPORT const unsigned char JSONParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONParser/PublicHeader.h>
+
+
